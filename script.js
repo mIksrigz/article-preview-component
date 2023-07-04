0622b009fccd511a70ee1f8853f5e00261044879
Mikolaj <m.iksrigz@gmail.com>
2023-07-04 14:57:34 +0200

    aasd

---
+++ script.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', () => {
+  const article = document.querySelector('.article');
+
+  const articleFooter1 = document.querySelector('#article-footer1');
+  const articleFooter2 = document.querySelector('#article-footer2');
+
+  article.addEventListener('click', e => {
+
+    if (e.target.matches('.arrow-container') || e.target.matches('.arrow')) {
+
+      if (window.innerWidth < 601) {
+        articleFooter1.classList.toggle('hide');
+        articleFooter2.classList.toggle('hide');
+      } else {
+
+      }
+
+    }
+  });
+
+});
